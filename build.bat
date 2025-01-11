414743cd7de96f581643f7a98450b52fa5568e4a
C42xe2 <erikdonath2006@gmail.com>
2025-01-11 22:09:51 +0100

    Added vscode build options. Removed old build files.

--- build.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-if "%~1"=="1" (
-    echo --^> Building Enviroment
-    docker build buildenv -t sodium-buildenv
-)
-if "%errorlevel%" == "0" (
-    echo --^> Building Sodium
-    docker run --rm -it -v %cd%:/root/env sodium-buildenv
-)
