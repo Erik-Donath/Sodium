817de6d190015910f289a49a0d8159777078b87f
Erik Donath <erikdonath@proton.me>
2026-03-21 20:38:44 +0100

    Fixed gdb seeing wrong path for source files.

--- src/arch/i686/pre_kernel.c
+++ src/arch/i686/pre_kernel.c
@@ -1,13 +1,18 @@
 #include "pre_kernel.h"
 #include "io.h"
 
-static void e9_print(const char* str) {
-    while (*str)
-        outb(0xE9, (uint8_t)*str++);
+static void e9_print(const char *str) {
+  while (*str)
+    outb(0xE9, (uint8_t)*str++);
 }
 
-void __attribute__((cdecl)) pre_kernel(void* mb_info) {
-    (void)mb_info;
-    e9_print("pre_kernel reached\n");
-    return; // Shutdown via booloader/i686/shutdown.asm
+void __attribute__((cdecl)) pre_kernel(void *mb_info) {
+  (void)mb_info;
+  e9_print("pre_kernel reached\n");
+
+  for (;;) {
+    e9_print("Hi\n");
+    e9_print("Ho\n");
+  }
+  return; // Shutdown via booloader/i686/shutdown.asm
 }
