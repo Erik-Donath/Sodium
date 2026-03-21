a6ee1bf04c4c433e1a1a53b808f273aca7d3c03c
Erik Donath <erikdonath@proton.me>
2026-03-21 13:18:51 +0100

    Deleted everything

--- src/arch/i686/boot/smbios/smbios.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-typedef const uint8_t* smbios_info_ptr;
-
-void smbios_print(smbios_info_ptr smbios, uint32_t size);
--- src/arch/i686/cpu/fpu/fpu.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include "../../../../kernel/libc/util.h"
-
-extern void ASM_CALL i686_FPU_Initialize();
