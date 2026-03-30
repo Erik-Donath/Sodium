222d5b3cfbc33c4a370d9878e5091473858af37a
Erik Donath <erikdonath@proton.me>
2026-03-30 02:10:15 +0200

    Testing VGA Output

--- src/arch/i686/pre_kernel.c
+++ src/arch/i686/pre_kernel.c
@@ -3,6 +3,7 @@
 
 // CPU Setup
 #include "gdt.h"
+#include <stdint.h>
 
 void __attribute__((cdecl)) pre_kernel(void *mb_info) {
   (void)mb_info;
@@ -10,14 +11,21 @@ void __attribute__((cdecl)) pre_kernel(void *mb_info) {
   // Seting up the CPU
   i686_gdt_init();
 
+  // Printing Sodium in aqua to VGA Output
+  static char *i686_vga = (char *)0xB8000;
+  const char *hello = "Sodium            ";
+  for (uint32_t i = 0; hello[i]; i++) {
+    i686_vga[(i << 1) + 0] = hello[i]; // Char
+    i686_vga[(i << 1) + 1] = 0x03;     // Color
+  }
+
   // Printing Sodium in aqua using ASCII Escape Seqenz to qemu debug output
   i686_io_debug("\033[38;5;6;48;5;0mSODIUM\033[0m\n");
 
-  // char *vga = (char *)0x8b000;
-
-  // vga[0] = 'H';
-
-  while (1)
+  // Wait some time by counting down
+  // I know this isn't the elegant Solution but not at the point of setting up a
+  // timer rn.This Code is for testing anyways
+  for (uint32_t i = INT32_MAX; i; i--)
     ;
 
   return; // Shutdown via booloader/i686/shutdown.asm
